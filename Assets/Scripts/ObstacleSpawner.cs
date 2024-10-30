33918642c3cb1249eeac6b235192691b5692f6a6
GabrielUgay <151297935+GabrielUgay@users.noreply.github.com>
2024-10-30 11:48:31 +0800

    Update ObstacleSpawner.cs

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+public class ObstaclesSpawner : MonoBehaviour
 {
-    public GameObject obstacle1, obstacle2;
+    public GameObject Obstacle1, Obstacle2, Obstacle3;
     [HideInInspector]
     public float obstacleSpawnInterval = 2.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("SpawnObstacles");
@@ -23,18 +22,21 @@ public class ObstacleSpawner : MonoBehaviour
         }
     }
 
-    private void SpawnObstacle()
+    public void SpawnObstacle()
     {
-        int random = Random.Range(1, 3);
-        
+        int random = Random.Range(1, 4);
+
         if (random == 1)
         {
-            Instantiate(obstacle1, new Vector3(transform.position.x, 0.2f, 0), Quaternion.identity);
+            Instantiate(Obstacle1, new Vector3(transform.position.x, -1.14f, 0), Quaternion.identity);
         }
-
-        else
+        else if (random == 2)
         {
-            Instantiate(obstacle2, new Vector3(transform.position.x, 0.2f, 0), Quaternion.identity);
+            Instantiate(Obstacle2, new Vector3(transform.position.x, -1.14f, 0), Quaternion.identity);
+        }
+        else if (random == 3)
+        {
+            Instantiate(Obstacle3, new Vector3(transform.position.x, -1.14f, 0), Quaternion.identity);
         }
     }
 
