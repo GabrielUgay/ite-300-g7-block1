8b401088f97ca40ce0c8bd2f69c448b7b6e94ec2
GabrielUgay <151297935+GabrielUgay@users.noreply.github.com>
2024-10-30 12:02:20 +0800

    Update GroundSpawner.cs

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -10,7 +10,7 @@ public class GroundSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -29,20 +29,21 @@ public class GroundSpawner : MonoBehaviour
 
         if (randomNum == 1)
         {
-            Instantiate(Ground1, new Vector3(transform.position.x + 10, 2f, 0), Quaternion.identity);
+            Instantiate(Ground1, new Vector3(transform.position.x + 14, 2.6f, 0), Quaternion.identity);
         }
         if (randomNum == 2)
         {
-            Instantiate(Ground2, new Vector3(transform.position.x + 15, -3.93f, 0), Quaternion.identity);
+            Instantiate(Ground2, new Vector3(transform.position.x + 14, 3.20f, 0), Quaternion.identity);
         }
         if (randomNum == 3)
         {
-            Instantiate(Ground3, new Vector3(transform.position.x + 15, 4.45f, 0), Quaternion.identity);
+            Instantiate(Ground3, new Vector3(transform.position.x + 14, 3.30f, 0), Quaternion.identity);
         }
         if (randomNum == 4)
         {
-            Instantiate(Ground4, new Vector3(transform.position.x - 15, -2f, 0), Quaternion.identity);
+            Instantiate(Ground4, new Vector3(transform.position.x + 14, 2.6f, 0), Quaternion.identity);
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -58,7 +59,6 @@ public class GroundSpawner : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             hasGround = false;
-
         }
     }
 }
