fa001629d6c3953c3af6296473e284930fa5a7d0
GabrielUgay <151297935+GabrielUgay@users.noreply.github.com>
2024-10-30 12:20:00 +0800

    Update ScoreSystem.cs

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,31 +1,94 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
 
 public class ScoreSystem : MonoBehaviour
 {
     public int score = 0;
-    public Text scoreText;
+    public TMP_Text scoreText;
+    private const string apiUrl = "http://127.0.0.1:8000/api/leaderboard";
 
-    // Start is called before the first frame update
+    private PlayerController playerController;
+    private int highScore;
+
     void Start()
     {
-        
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateScoreDisplay();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.isGameOver)
+        {
+            if (score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetInt("HighScore", highScore);
+                SaveScore();
+                Debug.Log("New High score is: " + highScore);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            score += 1;
-            scoreText.text = "Score: " + score;
+            IncrementScore();
         }
     }
+
+    private void IncrementScore()
+    {
+        score++;
+        UpdateScoreDisplay();
+    }
+
+    private void UpdateScoreDisplay()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void SaveScore()
+    {
+        string playerName = PlayerPrefs.GetString("PlayerName", "Unknown");
+        StartCoroutine(SaveScoreCoroutine(playerName));
+    }
+
+    private IEnumerator SaveScoreCoroutine(string playerName)
+    {
+        Debug.Log($"Sending to Laravel - Player Name: {playerName}, Score: {score}");
+
+        PlayerScoreData scoreData = new PlayerScoreData { player_name = playerName, score = score };
+        string jsonData = JsonUtility.ToJson(scoreData);
+
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(apiUrl, jsonData))
+        {
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(jsonData));
+            www.downloadHandler = new DownloadHandlerBuffer();
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error saving score: " + www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Server Response: " + responseText);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class PlayerScoreData
+{
+    public string player_name;
+    public int score;
 }
